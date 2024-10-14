9648d6fac2286fcf898e4a1046dc79f8b50f7901
hxzbaka <165454150+hxzbaka@users.noreply.github.com>
2024-10-14 22:04:56 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Patch init.c in order to shut down selinux
-patch -Np1 < device/sprd/sp9820e_1h10ll/init.c.patch
+patch -Np1 < device/sprd/sp9820e_1h10/init.c.patch
 
 add_lunch_combo omni_sp9820e_1h10-user
 add_lunch_combo omni_sp9820e_1h10-userdebug
